631f565d7c4b77bac04078158378575b4887d8e0
Tobias Fischer <info@tobiasfischer.info>
2022-03-21 09:31:48 +1000

    Fix for no GPS at end of file

--- extract_gps.py
+++ extract_gps.py
@@ -15,7 +15,7 @@ gpx = gpxpy.gpx.GPX()
 gpx_track = gpxpy.gpx.GPXTrack()
 gpx.tracks.append(gpx_track)
 
-for infile in glob.glob(os.path.join(args.path, 'GX*' + args.input + ".MP4")):
+for infile in sorted(glob.glob(os.path.join(args.path, 'GX*' + args.input + ".MP4"))):
     print('Process file ' + infile)
     stream = gpmf.io.extract_gpmf_stream(infile)
 
@@ -24,15 +24,26 @@ for infile in glob.glob(os.path.join(args.path, 'GX*' + args.input + ".MP4")):
 
     # Parse low level data into more usable format
     gps_data = list(map(gpmf.gps.parse_gps_block, gps_blocks))
-    last_invalid_gps_idx = 0
+    beginning_last_invalid_gps_idx = 0
+    ending_first_invalid_gps_idx = len(gps_data)
     for idx, gps_item in enumerate(gps_data):
         if gps_item.precision > 10 or gps_item.fix != 3:
-            last_invalid_gps_idx = idx + 1
-    print(f'Removing {last_invalid_gps_idx} (out of {len(gps_data)}) invalid GPS points, i.e.  {last_invalid_gps_idx / len(gps_data) * 100:.1f} %')
-    print(f'That\'s {(datetime.strptime(gps_data[last_invalid_gps_idx].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[0].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds out of {(datetime.strptime(gps_data[-1].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[0].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds')
-    gps_data = gps_data[last_invalid_gps_idx:]
+            if idx < int(len(gps_data) / 4):
+                beginning_last_invalid_gps_idx = idx + 1
+            elif idx > int(len(gps_data) * 3 / 4):
+                ending_first_invalid_gps_idx = idx
+                break
+            else:
+                print('WARNING: INVALID GPS MID ROUTE: ', idx, gps_item.precision, gps_item.fix)
+    print(f'Removing {beginning_last_invalid_gps_idx} (out of {len(gps_data)}) invalid GPS points at beginning, i.e.  {beginning_last_invalid_gps_idx / len(gps_data) * 100:.1f} %')
+    print(f'That\'s {(datetime.strptime(gps_data[beginning_last_invalid_gps_idx].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[0].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds out of {(datetime.strptime(gps_data[-1].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[0].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds')
+    print(f'Removing {len(gps_data) - ending_first_invalid_gps_idx} (out of {len(gps_data)}) invalid GPS points at the end, i.e.  {(len(gps_data) - ending_first_invalid_gps_idx) / len(gps_data) * 100:.1f} %')
+    print(f'That\'s {(datetime.strptime(gps_data[-1].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[ending_first_invalid_gps_idx-1].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds out of {(datetime.strptime(gps_data[-1].timestamp, "%Y-%m-%d %H:%M:%S.%f") - datetime.strptime(gps_data[0].timestamp, "%Y-%m-%d %H:%M:%S.%f")).total_seconds()} seconds')
+    gps_data = gps_data[beginning_last_invalid_gps_idx:ending_first_invalid_gps_idx]
 
     gpx_track.segments.append(gpmf.gps.make_pgx_segment(gps_data))
+
+assert len(gpx_track.segments) > 0, "No GPS data found"
 
 outfilename = os.path.join(args.path, args.input + '.gpx')
 print('Write gpx to ' + outfilename)
